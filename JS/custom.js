e09f536e70fdbe3ffde79a0a5e0c8884da1a8147
Emori03 <emorijan@gmail.com>
2024-02-23 09:53:44 -0600

    Modificacion de la forma en que se mostraba el login y registro

--- JS/custom.js
+++ JS/custom.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
+    menu("Ingreso");
+
     // Cargar el contenido HTML de un archivo externo
-    $('#contenido').load('./Modulos/Principal.html');
+    $('#contenido').load('./Modulos/Principal.html', function() {
+        console.log("cargo la funcion")
+            $('.filters_menu li').click(function () {
+            console.log("Le dio click")
+            $('.filters_menu li').removeClass('active');
+            $(this).addClass('active');
+    
+            var data = $(this).attr('data-filter');
+            $grid.isotope({
+                filter: data
+            })
+        });
+    
+        var $grid = $(".grid").isotope({
+            itemSelector: ".all",
+            percentPosition: false,
+            masonry: {
+                columnWidth: ".all"
+            }
+        })
+    });
 });
+
 // to get current year
 function getYear() {
     var currentDate = new Date();
@@ -11,64 +34,28 @@ function getYear() {
 
 getYear();
 
-
-// isotope js
-$(window).on('load', function () {
-    $('.filters_menu li').click(function () {
-        $('.filters_menu li').removeClass('active');
-        $(this).addClass('active');
-
-        var data = $(this).attr('data-filter');
-        $grid.isotope({
-            filter: data
-        })
-    });
-
-    var $grid = $(".grid").isotope({
-        itemSelector: ".all",
-        percentPosition: false,
-        masonry: {
-            columnWidth: ".all"
+function menu(modulo) {
+    var usuarioMenu = document.getElementById("usuarioMenu");
+    getMenu(modulo, function(elemento) {
+        if (elemento) {
+            usuarioMenu.innerHTML = elemento;
+        } else {
+            console.error('No se pudo cargar el elemento.');
         }
-    })
-});
-
-// nice select
-$(document).ready(function() {
-    $('select').niceSelect();
-  });
-
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    });
 }
 
-// client section owl carousel
-$(".client_owl-carousel").owlCarousel({
-    loop: true,
-    margin: 0,
-    dots: false,
-    nav: true,
-    navText: [],
-    autoplay: true,
-    autoplayHoverPause: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-        0: {
-            items: 1
+function getMenu(modulo, callback) {
+    console.log(modulo);
+    var url = './Modulos/' + modulo + '.html';
+    $.ajax({
+        url: url,
+        dataType: 'html',
+        success: function(data) {
+            callback(data);
         },
-        768: {
-            items: 2
-        },
-        1000: {
-            items: 2
+        error: function(xhr, status, error) {
+            console.error('Error al cargar el archivo modulo.html:', error);
         }
-    }
-});
+    });
+}
--- cafe.sql
+++ cafe.sql
@@ -82,7 +82,8 @@ CREATE TABLE `usuarios` (
   `Foto` longblob NOT NULL,
   `Correo` varchar(100) NOT NULL,
   `Telefono` varchar(20) NOT NULL,
-  `Genero` varchar(1) NOT NULL
+  `Genero` varchar(1) NOT NULL,
+  `Contrasena` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
