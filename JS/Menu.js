d37770489aa7d2a88cfc8e09c1e6adc89a29e95d
JorgeNeira87 <jorgeneira@969@gmail.com>
2024-02-29 12:36:46 -0600

    se modfico el mdal de pago y creo que ya

--- JS/Menu.js
+++ JS/Menu.js
@@ -1,51 +1,60 @@
 $('.pantalla-carga').fadeIn();
+var productos = new Array();
+Promise.all([obtenerProductos()])
+    .then(valores => {
+        productos = valores[0];
+        cargarProductos(productos);
+        $('.pantalla-carga').fadeOut();
 
-Promise.all([cargarProductos()])
-                .then(valores => {
-                    var menu = document.getElementById("menuDiv");
-                    var divMenu = valores[0];
-                    menu.appendChild(divMenu);
-                    $('.pantalla-carga').fadeOut();
+        $('.filters_menu li').click(function () {
+            $('.filters_menu li').removeClass('active');
+            $(this).addClass('active');
 
-                    $('.filters_menu li').click(function () {
-                        $('.filters_menu li').removeClass('active');
-                        $(this).addClass('active');
+            var data = $(this).attr('data-filter');
+            $grid.isotope({
+                filter: data
+            })
+        });
 
-                        var data = $(this).attr('data-filter');
-                        $grid.isotope({
-                            filter: data
-                        })
-                    });
-
-                    var $grid = $(".grid").isotope({
-                        itemSelector: ".all",
-                        percentPosition: false,
-                        masonry: {
-                            columnWidth: ".all"
-                        }
-                    })
-                })
-                .catch(error => {
-                    console.error("Ocurrió un error:", error);
-                });
-
-
-async function cargarProductos() {
+        var $grid = $(".grid").isotope({
+            itemSelector: ".all",
+            percentPosition: false,
+            masonry: {
+                columnWidth: ".all"
+            }
+        })
+    })
+    .catch(error => {
+        console.error("Ocurrió un error:", error);
+    });
+    
+async function obtenerProductos() {
     try {
+        
         const response = await $.ajax({
             url: './PHP/Menu.php',
             type: 'GET',
             contentType: 'application/json'
         });
 
-        // Crear el div
-        var div = document.createElement("div");
-        div.classList.add("row", "grid", "p-b-1");
+        return response;
+        
+    } catch (error) {
 
-        // Llenar el div con contenido basado en la respuesta
-        for (let i = 0; i < response.length; i++) {
+        console.error('Error en la solicitud:', error);
+        throw error;
+    }
+}
+function cargarProductos(noesmenu) {
+    
+    try {
+        var menu = document.getElementById("menuDiv");
+        var div = document.createElement("div");
+        div.classList.add("row", "p-b-1");
+        
+    for (let i = 0; i < noesmenu.length; i++) {
             var divFiltro = document.createElement("div");
-            divFiltro.classList.add(response[i].Tipo, "col-sm-6", "col-lg-4", "all");
+            divFiltro.classList.add(noesmenu[i].Tipo, "col-sm-6", "col-lg-4", "all");
 
             var divCard = document.createElement("div");
             divCard.classList.add("p-1", "card", "bg-dark", "rounded");
@@ -53,7 +62,7 @@ async function cargarProductos() {
 
             var img = document.createElement("img");
             img.classList.add("rounded-bottom", "bg-white", "mh-100", "img-fluid");
-            img.src = "Imagenes/" + response[i].Nombre + ".png";
+            img.src = "Imagenes/" + noesmenu[i].Nombre + ".png";
             img.style.height = "40vh";
             divCard.appendChild(img);
 
@@ -62,21 +71,28 @@ async function cargarProductos() {
 
             var hNombre = document.createElement("h5");
             hNombre.classList.add("card-title", "text-white");
-            hNombre.textContent = response[i].Nombre;
+            hNombre.textContent = noesmenu[i].Nombre;
             divCardBody.appendChild(hNombre);
 
             var pDescripcion = document.createElement("p");
             pDescripcion.classList.add("card-text", "text-white");
-            pDescripcion.textContent = response[i].Descripcion;
+            pDescripcion.textContent = noesmenu[i].Descripcion;
             divCardBody.appendChild(pDescripcion);
 
             var hPrecio = document.createElement("h6");
             hPrecio.classList.add("card-subtitle", "text-white");
-            hPrecio.textContent = "$"+ response[i].Precio;
+            hPrecio.textContent = "$" + noesmenu[i].Precio;
             divCardBody.appendChild(hPrecio);
 
             var btnCart = document.createElement("buttom");
             btnCart.classList.add("btn", "btn-warning", "position-absolute", "bottom-0", "end-0");
+            btnCart.id = noesmenu[i].ProductoId;
+            btnCart.setAttribute("data-bs-toggle", "modal");
+            btnCart.setAttribute("data-bs-target", "#moproducto");
+            btnCart.onclick = function () {
+                idToModal(this);
+
+            };
 
             var iCart = document.createElement("i");
             iCart.classList.add("bi", "bi-cart-fill");
@@ -89,9 +105,21 @@ async function cargarProductos() {
             divFiltro.appendChild(divCard);
             div.appendChild(divFiltro);
         }
-        return div;
+        menu.appendChild(div);
+
     } catch (error) {
         console.error('Error en la solicitud:', error);
         throw error;
+    }
+}
+
+function idToModal(Producto) {
+    var ProductoId = Producto.id;
+    console.log(ProductoId);
+    console.log(productos);
+    for(let i = 0; i < productos.length; i++){
+        if(productos[i].ProductoId == ProductoId){
+            console.log(productos[i]);
+        }
     }
 }
--- JS/custom.js
+++ JS/custom.js
@@ -1,95 +1,71 @@
 $(document).ready(function () {
-    setTimeout(function() {
-        $('.pantalla-carga').fadeOut();
-      }, 2000);
-    menu("Ingreso");
+  setTimeout(function () {
+    $('.pantalla-carga').fadeOut();
+  }, 2000);
+  menu("Ingreso");
 
-    // Cargar el contenido HTML de un archivo externo
-    modulo("Principal");
-    
+  // Cargar el contenido HTML de un archivo externo
+  modulo("Principal");
+
 });
 
 function modulo(modulo) {
-    $('#contenido').load('./Modulos/' + modulo + '.html', function () {
-        if (modulo == "Menu") {
-            var carrusel = document.getElementsByClassName("slider_section");
-            $(carrusel).addClass("d-none");
-        } else if (modulo == "Principal") {
-            var carrusel = document.getElementsByClassName("slider_section");
-            $(carrusel).removeClass("d-none");
-        }
-    });
+  $('#contenido').load('./Modulos/' + modulo + '.html', function () {
+    if (modulo == "Menu") {
+      var carrusel = document.getElementsByClassName("slider_section");
+      $(carrusel).addClass("d-none");
+    } else if (modulo == "Principal") {
+      var carrusel = document.getElementsByClassName("slider_section");
+      $(carrusel).removeClass("d-none");
+    }
+  });
 }
 
 // to get current year
 function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  document.querySelector("#displayYear").innerHTML = currentYear;
 }
 
 getYear();
 
 function menu(modulo) {
-    $('#usuarioMenu').load('./Modulos/' + modulo + '.html', function () {
-    });
+  $('#usuarioMenu').load('./Modulos/' + modulo + '.html', function () {
+  });
 }
 
 function ingreso() {
-    $.ajax({
-        url: 'archivo_destino.php',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            clave1: 'valor1',
-            clave2: 'valor2'
-        }),
-        success: function (response) {
-            console.log('Respuesta del servidor:', response);
-        },
-        error: function (xhr, status, error) {
-            console.error('Error en la solicitud:', error);
-        }
-    });
+  $.ajax({
+    url: 'archivo_destino.php',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      clave1: 'valor1',
+      clave2: 'valor2'
+    }),
+    success: function (response) {
+      console.log('Respuesta del servidor:', response);
+    },
+    error: function (xhr, status, error) {
+      console.error('Error en la solicitud:', error);
+    }
+  });
 }
 
 function selectPaymentMethod(method) {
-    var paymentForm = document.getElementById("paymentForm");
 
-    if (method === 'paypal') {
-        paymentForm.innerHTML = `
-        <br>   
-        <p>Por favor, inicie sesión en su cuenta de PayPal para completar el pago.</p>
-        <form id="paypalForm">
-          <div class="form-group">
-            <label for="paypalUsername">Nombre de usuario:</label>
-            <input type="text" class="form-control" id="paypalUsername" placeholder="Ingrese su nombre de usuario de PayPal">
-          </div>
-          <br>
-          <div class="form-group">
-            <label for="paypalPassword">Contraseña:</label>
-            <input type="password" class="form-control" id="paypalPassword" placeholder="Ingrese su contraseña de PayPal">
-          </div>
-        </form>`;
-    } else if (method === 'credit_card') {
-        paymentForm.innerHTML = `
-        <br>   
-        <p>Por favor, ingrese los detalles de su tarjeta de crédito para completar el pago.</p>
-        <form id="creditCardForm">
-          <div class="form-group">
-            <label for="cardNumber">Número de Tarjeta:</label>
-            <input type="text" class="form-control" id="cardNumber" placeholder="Ingrese el número de su tarjeta de crédito">
-          </div>
-          <br>
-          <div class="form-group">
-            <label for="expiryDate">Fecha de Expiración:</label>
-            <input type="text" class="form-control" id="expiryDate" placeholder="MM/YY">
-          </div>
-          <br>
-          <div class="form-group">
-            <label for="cvv">CVV:</label>
-            <input type="text" class="form-control" id="cvv" placeholder="Ingrese el CVV">
-          </div>
-        </form>`;
-    }
+  if (method === 'paypal') {
+    $('#paymentForm').load('./Modulos/Paypal.html', function () {
+    });
+
+  } else if (method === 'credit_card') {
+    $('#paymentForm').load('./Modulos/Credito.html', function () {
+    });
+  }
+}
+
+function lamagiadelmodal(){
+  var paymentForm = document.getElementById("paymentForm");
+  paymentForm.innerHTML="";
 }
